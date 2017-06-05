0880af58983d8dc0b37235f354785b42477ffdc4
Brian Sloan <bsloan@2u.com>
2017-06-04 22:34:25 -0400

    start refactoring the indexers

---
+++ es-esa/indexer/settings.py
@@ -0,0 +1,5 @@
+# Elasticsearch (AWS service) settings
+
+AWS_ELASTICSEARCH_HOST = "search-es-esa-mmwke2z5lwruewstytctb4e6ka.us-east-1.es.amazonaws.com"
+AWS_ELASTICSEARCH_REGION = "us-east-1"
+AWS_ELASTICSEARCH_PORT = 80
